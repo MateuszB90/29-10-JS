02fdd842be52b4c845eaab79784f832ef3377842
MateuszB90 <116488337+MateuszB90@users.noreply.github.com>
2022-10-30 15:22:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1main = document.getElementById("title");
+h1main.textContent = "Good Aftermoon"
+
+
+
+const h3main = document.getElementById("subTitle");
+h3main.textContent = "Hello Mateusz"
